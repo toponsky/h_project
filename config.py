01d1e88eec5b1ccccd21656f157a5bce16f0d448
yliu <yliu@vectra.ai>
2023-02-13 12:10:21 +0000

    Init pull

---
+++ config.py
@@ -0,0 +1,14 @@
+urls = {
+  'German': 'https://www.hermes.com/de/de/category/damen/taschen-und-kleinlederwaren/taschen-und-kleine-taschen/',
+  'IE': 'https://www.hermes.com/ie/en/category/women/bags-and-small-leather-goods/bags-and-clutches'
+}
+
+bag_names=[
+  'TASCHE LINDY 26'
+]
+
+
+db_connection = {
+  'username': 'yl_root',
+  'password': 'Cc00114110!'
+}
---
+++ main.py
@@ -0,0 +1,25 @@
+from bs4 import BeautifulSoup as BS
+from lxml import html
+import requests
+import config
+from DBManagement import Database as Database
+
+def soupify(url, bags, db):
+    page = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'},verify=False)
+    soup = BS(page.text, 'lxml')
+
+    # allbags = soup.find_all('h3', {'class':'product-item-name'})
+    allBags = soup.find_all('div', {'product-grid-list-item'})
+    import pdb; pdb.set_trace()
+    for b in allBags:
+        print(b.text)
+
+    db.insertOneBag("p_id", "name", "imageURL", "url")
+    print("I am here")
+
+if __name__ == "__main__":
+             
+    db = Database.Database(config.db_connection.username, config.db_connection.password)
+    for country in config.urls:
+        soupify(config.urls[country], config.bag_names, db)
+      
