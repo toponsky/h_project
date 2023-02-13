b9b5ca0943af49d52c06c135daeb5410bc0cc14f
yliu <yliu@vectra.ai>
2023-02-13 17:27:30 +0000

    update

--- main.py
+++ main.py
@@ -5,14 +5,14 @@ import config
 from DBManagement import Database as Database
 
 def soupify(url, bags, db):
-    # page = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'},verify=False, proxies=proxy)
-    # soup = BS(page.text, 'lxml')
+    page = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'},verify=False, proxies=proxy)
+    soup = BS(page.text, 'lxml')
 
-    # # allbags = soup.find_all('h3', {'class':'product-item-name'})
-    # allBags = soup.find_all('div', {'product-grid-list-item'})
-    # import pdb; pdb.set_trace()
-    # for b in allBags:
-    #     print(b.text)
+    # allbags = soup.find_all('h3', {'class':'product-item-name'})
+    allBags = soup.find_all('div', {'product-grid-list-item'})
+    import pdb; pdb.set_trace()
+    for b in allBags:
+        print(b.text)
 
     db.insertOneBag("p_id", "name", "imageURL", "url")
     print("I am here")
